8ad76ac32c02cf02356b00bd5ddf33a61ead020e
Ubong Benjamin <ubongben816@gmail.com>
2022-07-14 20:10:20 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFolder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I love you Lord!");
+        }
+    }
+}
